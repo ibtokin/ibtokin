fcea6bf51d02eb2b578132eaffe0ddf5d2bd601d
Adan Sandoval <adansandoval@Adans-MacBook-Pro.local>
2016-11-18 16:56:34 -0600

    updated url paths or something

--- jawns/urls.py
+++ jawns/urls.py
@@ -4,5 +4,4 @@ from jawns.views import  ContentDetailView, ContentListView
 urlpatterns = [
     url(r'^$', ContentListView.as_view(), name='content-list'),
     url(r'^(?P<slug>[-\w]+)/$', ContentDetailView.as_view(), name='content_detail'),
-
 ]
