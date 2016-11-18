76c7a0adb8ba92a2827450df0b46399373c01c39
Adan Sandoval <adansandoval@Adans-MacBook-Pro.local>
2016-11-18 16:20:19 -0600

    Merge conflict

--- jawns/urls.py
+++ jawns/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import url
-<<<<<<< HEAD
-from .views import  ContentDetailView, ContentListView
-
-=======
 from jawns.views import  ContentDetailView, ContentListView
->>>>>>> cdb1cb6b20af189919aa977d8d5995e3bc087c9f
 
 urlpatterns = [
     url(r'^$', ContentListView.as_view(), name='content-list'),
