1dc0d570988e42a178d27901ef89c695b6f56860
Adan Sandoval <adansandoval@Adans-MacBook-Pro.local>
2016-11-18 15:20:11 -0600

    Added They Models to Jawns app

--- jawns/models.py
+++ jawns/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+'''
+Note: these models are not supposed to make sense.
+In the event that you do find meaning in them,
+consult your doctor.
+'''
+class They(models.Model):
+    title = models.Charfield(max_length=100)
+    date = models.DateTimeField('Created')
+
+
+class Content(models.Model):
+    they = models.ForeignKey(They, on_delete=models.CASCADE)
+    description = models.Charfield(max_length=200)
+    url = models.URLField()
+    image_url = models.URLField()
+    count = models.IntegerField(default=0)
